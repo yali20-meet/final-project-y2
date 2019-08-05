37a1969060d484e039b3cde826eb6194e32c2681
myname21-meet <myname21@meet.mit.edu>
2019-08-05 02:24:57 -0400

    connect css to html

---
+++ dataBase-final.py
@@ -0,0 +1,9 @@
+from model import Base, Student
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///lecture.db')
+Base.metadata.create_all(engine)
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
