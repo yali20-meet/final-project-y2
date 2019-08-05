db51136056e4f58d5c4c3e070823758e1efc2e64
myname21-meet <myname21@meet.mit.edu>
2019-08-05 02:28:52 -0400

    added static and style.css

--- app-final.py
+++ app-final.py
@@ -2,9 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home_route():
     return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
-
