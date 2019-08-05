62562c651369c86bb904a3469247219ef385f39e
myname21-meet <myname21@meet.mit.edu>
2019-08-05 09:08:04 -0400

    added a shit ton of stuff to index and style

--- app-final.py
+++ app-final.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +7,4 @@ def home_route():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(debug=True, port=9000)
