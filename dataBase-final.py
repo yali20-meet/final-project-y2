f6f2c7da21e61d39de38b0d913f0a5655c2a6643
myname21-meet <myname21@meet.mit.edu>
2019-08-06 08:42:21 -0400

    finito

--- dataBase-final.py
+++ dataBase-final.py
@@ -16,6 +16,3 @@ def add_company(companyName , email , companyInfo):
 def get_company(name):
 	company = session.query(Company).filter_by(companyName = name).first()
 	return company
-
-
-print(get_company("google"))
